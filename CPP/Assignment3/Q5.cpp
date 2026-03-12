44cbc2c882d0470df76541ec9d461b8c27c3500b
Hrutik1214 <hrutikshinde9555@gmail.com>
2026-03-12 15:04:51 +0530

    asignments added

---
+++ CPP/Assignment3/Q5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int add(int a = 10, int b = 20)
+{
+    return a + b;
+}
+
+int main()
+{
+    cout << add() << endl;
+    cout << add(50) << endl;
+    cout << add(0,20) << endl;
+
+    return 0;
+}
